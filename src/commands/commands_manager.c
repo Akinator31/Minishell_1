9635464ebb2842eed22095be7dba65eb55dfa67f
Pavel <pavel.de-wavrechin@epitech.eu>
2025-02-10 10:21:48 +0100

    feat(commands_manager): add command manager to handle all the mandatory commands

---
+++ src/commands/commands_manager.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** minishell_1
+** File description:
+** commands_manager
+*/
+
+#include <unistd.h>
+#include "my_lib.h"
+#include "commands.h"
+#include "mysh.h"
+
+extern char **environ;
+
+int analyse_command(char *command, bool *environ_modified)
+{
+    if (is_exit_command(command))
+        return EXIT;
+    if (is_nothing(command))
+        return NOTHING;
+    if (is_setenv_command(command, environ_modified)) {
+        write(1, prompt, 3);
+        return NORMAL;
+    }
+    if (is_unsetenv_command(command, environ_modified)) {
+        write(1, prompt, 3);
+        return NORMAL;
+    }
+    if (is_env_command(command)) {
+        write(1, prompt, 3);
+        return NORMAL;
+    }
+    write(1, prompt, 3);
+    return NORMAL;
+}
