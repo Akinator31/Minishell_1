f4e62b7360c6024d93c2e484edd666df490a522c
Pavel <pavel.de-wavrechin@epitech.eu>
2025-02-12 12:34:59 +0100

    refactor(main): remove 'mysh_tty' function by adding a condition and an 'is_tty' parameter to 'mysh' function

--- src/main.c
+++ src/main.c
@@ -9,40 +9,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
 #include "my_lib.h"
 #include "mysh.h"
 #include "utils.h"
 
-int mysh_tty(char ***envp)
+int mysh(char ***envp, int is_tty)
 {
     int result_command = 0;
     char *buffer = NULL;
     size_t len = 0;
 
+    if (!is_tty)
+        print_prompt(*envp);
     while (getline(&buffer, &len, stdin) != -1) {
-        result_command = analyse_command(envp, buffer, 1);
-        if (result_command == EXIT)
+        result_command = analyse_command(envp, buffer, is_tty);
+        if (result_command == EXIT) {
+            free(buffer);
             return EXIT;
+        }
         if (result_command == NOTHING)
             continue;
     }
-    return EXIT_EOF;
-}
-
-int mysh(char ***envp)
-{
-    int result_command = 0;
-    char *buffer = NULL;
-    size_t len = 0;
-
-    write(1, prompt, 4);
-    while (getline(&buffer, &len, stdin) != -1) {
-        result_command = analyse_command(envp, buffer, 0);
-        if (result_command == EXIT)
-            return EXIT;
-        if (result_command == NOTHING)
-            continue;
-    }
+    free(buffer);
     return EXIT_EOF;
 }
 
@@ -54,9 +43,9 @@ int main(int ac, char **av, char **envp)
 
     env = duplicate_2d_char_array(envp, get_2d_arr_len(envp) + 1);
     if (!isatty(stdin->_fileno))
-        mysh_tty_exit_status = mysh_tty(&env);
+        mysh_tty_exit_status = mysh(&env, 1);
     else
-        mysh_exit_status = mysh(&env);
+        mysh_exit_status = mysh(&env, 0);
     if (mysh_exit_status == EXIT_EOF)
         write(1, "exit\n", 6);
     free_2d_array_of_char(env);
