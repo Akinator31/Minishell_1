e0faf61e00608f579adaf85788f19ef121701914
Pavel <pavel.de-wavrechin@epitech.eu>
2025-02-12 12:36:03 +0100

    fix(is_good_command): fix one memory leak in 'is_good_command' function

--- src/utils/is_good_command.c
+++ src/utils/is_good_command.c
@@ -14,8 +14,7 @@ bool is_good_cmd(char *cmd, char *prompt)
     char **prompt_elt = my_str_to_word_array(prompt, " ");
     int is_correct_cmd = my_strcmp(cmd, prompt_elt[0]);
 
-    if (is_correct_cmd == IS_A_TTY_OFFSET ||
-        is_correct_cmd == IS_NOT_A_TTY_OFFSET) {
+    if (is_correct_cmd == 0) {
         free_2d_array_of_char(prompt_elt);
         return true;
     }
