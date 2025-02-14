78ef8853f89d6c60db1b827aa788abc34b11b106
Pavel <pavel.de-wavrechin@epitech.eu>
2025-02-14 14:13:03 +0100

    fix(my_memset): switch iterator type to size_t

--- lib/my_memset.c
+++ lib/my_memset.c
@@ -9,7 +9,7 @@
 
 void *my_memset(void *pointer, int value, size_t size)
 {
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         ((char *)pointer)[i] = value;
     }
     return pointer;
